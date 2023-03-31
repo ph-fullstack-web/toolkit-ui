018c6d540588acfe99230fffcae2f9ad8de4ec4f
SVPH\dranoel.jemina <dranoel.jemina@softvision.com>
2023-03-31 22:11:56 +0800

    Add JWT Service Unit test

---
+++ src/app/core/services/jwt.service.spec.ts
@@ -0,0 +1,36 @@
+import { TestBed } from '@angular/core/testing';
+import { JwtService } from './jwt.service';
+
+
+describe('JwtService', () => {
+
+  let service: JwtService;
+
+  beforeEach(() => {
+  
+      const apiServiceSpyObj = jasmine.createSpyObj('ApiService', ['get', 'post', 'delete']);
+      TestBed.configureTestingModule({providers: [
+        JwtService, 
+      ]});
+
+  });
+
+  it ('getToken should return mocked token', () => {
+      service = TestBed.inject(JwtService);
+      window.localStorage.setItem('jwtToken', 'myToken');
+      expect(service.getToken()).toEqual('myToken');
+  }); 
+
+  it ('saveToken should save mocked token', () => {
+      service = TestBed.inject(JwtService);
+      service.saveToken('myToken_1');
+      expect(window.localStorage['jwtToken']).toEqual('myToken_1');
+  }); 
+
+  it ('destroyToken should remove mocked token', () => {
+    service = TestBed.inject(JwtService);
+    service.destroyToken();
+    expect(window.localStorage['jwtToken']).toBeNull;
+  }); 
+
+});
