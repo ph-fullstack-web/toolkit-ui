ec2108eadf3ed27f9a5b433e423282728477d204
Paul Christian Jimenez <polxtian89@gmail.com>
2023-04-03 00:32:35 +0800

    setup test files configuration

---
+++ karma.config.js
@@ -0,0 +1,48 @@
+module.exports = function (config) {
+  config.set({
+    basePath: "",
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
+    plugins: [
+      require("karma-jasmine"),
+      require("@chiragrupani/karma-chromium-edge-launcher"),
+      require("karma-chrome-launcher"),
+      require("karma-jasmine-html-reporter"),
+      require("karma-coverage"),
+      require("@angular-devkit/build-angular/plugins/karma"),
+    ],
+    client: {
+      /** Leave jasmine spec runner output visible in browser. */
+      clearContext: false,
+      // captureConsole: false, -> Prod settings
+    },
+    jasmineHtmlReporter: {
+      /** removes the duplicated traces. */
+      supressAll: true,
+    },
+    coverageReporter: {
+      dir: require("path").join(__dirname, "./coverage/"),
+      subdir: ".",
+      reporters: [{ type: "html" }, { type: "text-summary" }],
+      check: {
+        global: {
+          statement: 50,
+          branches: 50,
+          functions: 50,
+          lines: 50,
+        },
+      },
+    },
+    reporters: ["progress", "kjhtml"],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    /**
+     * Change the value to ["Chrome"],
+     * I'm using Edge since I don't have Chrome installed.
+     */
+    browsers: ["Edge"],
+    browserNoActivityTimeout: 2000,
+    singleRun: false,
+  });
+};
