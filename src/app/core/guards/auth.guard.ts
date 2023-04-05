2c563b7fe2c70852f7a0c64215ab134ac7d0da07
Paul Christian Jimenez <polxtian89@gmail.com>
2023-04-05 19:31:26 +0800

    try husky hooks

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  RouterStateSnapshot,
+} from '@angular/router';
 import { Observable, take } from 'rxjs';
 
 import { AppStore } from '@app/store';
@@ -11,7 +15,10 @@ import { AuthSelectors } from '@app/store/auth';
 export class AuthGuard implements CanActivate {
   constructor(private store: AppStore) {}
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> {
     return this.store.select(AuthSelectors.isAuthenticated).pipe(take(1));
   }
 }
