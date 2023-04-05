27ee2028525d7b47d6cfac533c11cb6d9f2e4640
Paul Christian Jimenez <polxtian89@gmail.com>
2023-04-05 22:24:58 +0800

    test hook

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -7,7 +7,13 @@ import { HttpTokenInterceptor } from '@interceptors';
 @NgModule({
   declarations: [],
   imports: [CommonModule, HttpClientModule],
-  providers: [{ provide: HTTP_INTERCEPTORS, useClass: HttpTokenInterceptor, multi: true }],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpTokenInterceptor,
+      multi: true,
+    },
+  ],
   exports: [CommonModule, HttpClientModule],
 })
 export class CoreModule {}
