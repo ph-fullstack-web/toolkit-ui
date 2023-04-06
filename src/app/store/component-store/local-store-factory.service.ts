e711043030d96d9c41ffd30e658bb2e52636df1f
Paul Christian Jimenez <polxtian89@gmail.com>
2023-04-06 13:59:44 +0800

    fix eslint error in factory and remove ngrx/component-store dependency.

--- src/app/store/component-store/local-store-factory.service.ts
+++ src/app/store/component-store/local-store-factory.service.ts
@@ -1,14 +1,12 @@
-import { Injectable, Injector, Type, Provider } from '@angular/core';
-
+import { Injectable, Injector, Provider, ClassProvider } from '@angular/core';
 import {
-  ConsultantStore,
   LocalModel,
   LocalState,
   LocalStore,
-  ProfileStore,
   StoreName,
+  provideConsultantStore,
+  provideProfileStore,
 } from '@app/store/local';
-import { provideComponentStore } from '@ngrx/component-store';
 
 @Injectable()
 export class LocalStoreFactory {
@@ -19,29 +17,34 @@ export class LocalStoreFactory {
     TState extends LocalState<LocalModel> = LocalState<LocalModel>,
     TModel extends LocalModel = LocalModel
   >(storeName: StoreName): TLocalStore {
-    const storeType = this.getStoreType<TLocalStore>(storeName);
+    /**
+     * https://github.com/ngrx/platform/blob/master/modules/component-store/src/lifecycle_hooks.ts
+     * the localStoreProviders array contains:
+     * [0] - provider for the local store
+     * [1] - provider for CS_WITH_HOOKS to call the useFactory and execute the hooks.
+     */
+    const localStoreProviders = this.getStoreProviders(storeName);
+    const [provider] = localStoreProviders;
+
     const injector = Injector.create({
       parent: this.injector,
       /** There should ONLY be 1 local store that will be added to DI + the root (parent injector). */
-      providers: [provideComponentStore(storeType as Type<any>)],
+      providers: [localStoreProviders],
     });
 
-    const store = injector.get(storeType);
+    const store = injector.get((provider as ClassProvider).useClass);
 
     store.initializeState();
 
     return store;
   }
 
-  private getStoreType<TLocalStore>(storeName: StoreName): Type<TLocalStore> {
-    /** TODO: remove type 'any' */
-    const storeConstructorMap = new Map<StoreName, any>([
-      /** Map store name & derived store class type pair below.. */
-      ['profile', ProfileStore],
-      ['consultant', ConsultantStore],
+  private getStoreProviders(storeName: StoreName): Provider[] {
+    const storeConstructorMap = new Map<StoreName, Provider[]>([
+      ['profile', provideProfileStore()],
+      ['consultant', provideConsultantStore()],
     ]);
-
-    return storeConstructorMap.get(storeName) as Type<TLocalStore>;
+    return storeConstructorMap.get(storeName)!;
   }
 }
 
